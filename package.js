72107c755aa100bf9df201ba39ad3e8220f22330
Abigail Watson <awatson1978@gmail.com>
2016-04-03 22:22:20 -0700

    bump and publish

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'clinical:hl7-resource-diagnostic-order',
-  version: '1.3.3',
+  version: '1.4.0',
   summary: 'HL7 FHIR Resource - Diagnostic Order',
   git: 'https://github.com/clinical-meteor/hl7-resource-diagnostic-order',
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
   api.use('meteor-platform');
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
   api.use('aldeed:collection2@2.5.0');
   api.use('clinical:hl7-resource-datatypes@0.2.1');
   api.use('simple:json-routes@2.1.0');
-  api.use('prime8consulting:meteor-oauth2-server@0.0.2')
+  api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
 
   api.addFiles('lib/hl7-resource-diagnostic-order.js');
@@ -26,7 +26,7 @@ Package.onUse(function(api) {
   api.export('DiagnosticOrders');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('tinytest');
   api.use('clinical:hl7-resource-diagnostic-order');
 });
